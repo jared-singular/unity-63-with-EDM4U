8fbcffd7a4acee4b198fcb90a685e6c9e8d50ca8
Jared Ornstead <jared@singular.net>
2026-01-16 13:44:30 -0800

    Fixes to IOSPodInstallPostBuild

--- unity-63-with-EDM4U/Assets/Editor/IOSPodInstallPostBuild.cs
+++ unity-63-with-EDM4U/Assets/Editor/IOSPodInstallPostBuild.cs
@@ -1,5 +1,6 @@
 #if UNITY_EDITOR && UNITY_IOS
 using System.Diagnostics;
+using System.IO;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEngine;
@@ -12,13 +13,29 @@ public static class IOSPodInstallPostBuild
         if (target != BuildTarget.iOS)
             return;
 
+        // Unity build path is the Builds folder itself
+        var buildsDir   = pathToBuiltProject;
+        var podfilePath = Path.Combine(buildsDir, "Podfile");
+
+        if (!File.Exists(podfilePath))
+        {
+            UnityEngine.Debug.Log("[iOSPodInstallPostBuild] No Podfile found in " + buildsDir + ", skipping pod install.");
+            return;
+        }
+
         var process = new Process();
         process.StartInfo.FileName = "/bin/bash";
 
-        // Use -lc so it loads your shell env (PATH with Homebrew Ruby + pod)
-        process.StartInfo.Arguments =
-            "-lc \"cd '" + pathToBuiltProject + "' && pod install --repo-update\"";
+        const string rubyBin = "/opt/homebrew/opt/ruby/bin";
+        const string gemBin  = "/opt/homebrew/lib/ruby/gems/4.0.0/bin"; // adjust if Gem.bindir differs
 
+        var cmd =
+            "export LANG=en_US.UTF-8; " +
+            "export LC_ALL=en_US.UTF-8; " +
+            "export PATH=\"" + rubyBin + ":" + gemBin + ":$PATH\"; " +
+            "cd '" + buildsDir + "' && pod install --repo-update";
+
+        process.StartInfo.Arguments = "-lc \"" + cmd + "\"";
         process.StartInfo.UseShellExecute = false;
         process.StartInfo.RedirectStandardOutput = true;
         process.StartInfo.RedirectStandardError = true;
@@ -26,7 +43,7 @@ public static class IOSPodInstallPostBuild
         process.Start();
 
         string output = process.StandardOutput.ReadToEnd();
-        string error = process.StandardError.ReadToEnd();
+        string error  = process.StandardError.ReadToEnd();
 
         process.WaitForExit();
 
